6bc80f8be7137a731bbf1a194732c5d3910fba70
Tay Kong Yu <kongyutay@gmail.com>
2024-12-12 21:06:54 +0800

    done forward props

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,10 +1,12 @@
-export default function TabButton({ children, onSelect, isSelected }) {
-  console.log('TABBUTTON COMPONENT EXECUTING');
-  return (
-    <li>
-      <button className={isSelected ? 'active' : undefined} onClick={onSelect}>
-        {children}
-      </button>
-    </li>
-  );
+export default function TabButton({ children, isSelected, ...props }) {
+    console.log("TABBUTTON COMPONENT EXECUTING");
+    return (
+        // TabButton 这里直接用...props 代替了onSelect 的custom prop，进行传递。
+        // 在父组件那里就要移除onSelect的自定义prop，改成onClick，因为可以直接继承
+        <li>
+            <button className={isSelected ? "active" : undefined} {...props}>
+                {children}
+            </button>
+        </li>
+    );
 }
