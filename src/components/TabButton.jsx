09928b483541ffa3b47d7b1d1ae3a40f35168665
Tay Kong Yu <kongyutay@gmail.com>
2024-12-12 14:26:28 +0800

    output list data

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,11 +1,10 @@
-export default function TabButton({ children }) {
-    function handleClick() {
-        console.log("Hello World!");
-    }
-
-    return (
-        <li>
-            <button onClick={handleClick}>{children}</button>
-        </li>
-    );
+export default function TabButton({ children, onSelect, isSelected }) {
+  console.log('TABBUTTON COMPONENT EXECUTING');
+  return (
+    <li>
+      <button className={isSelected ? 'active' : undefined} onClick={onSelect}>
+        {children}
+      </button>
+    </li>
+  );
 }
